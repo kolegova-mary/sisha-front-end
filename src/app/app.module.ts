a48cab2d7277c772a75a77548c830e5e1c425599
Masha <kolegovamasha@mail.ru>
2020-01-07 14:38:11 +0300

    g

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { LoginComponent } from './login/login.component';
 import { FormsModule } from '../../node_modules/@angular/forms';
 import { HttpClientModule } from '../../node_modules/@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    DashboardComponent,
+    HeaderComponent,
+    MenuComponent,
+    RegisterComponent,
 
   ],
   imports: [
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,4 +26,7 @@ export class LoginComponent implements OnInit {
     });
    f.reset();
   }
+  clicked(event){
+    
+  }
 }
